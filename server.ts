a50bbf7bb3a60e14c04bcc6b5d418682017ca71a
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-04-21 11:15:26 +0200

    prettier

--- server.ts
+++ server.ts
@@ -1,16 +1,16 @@
-import * as path from 'path';
-import * as fs from 'fs';
+import * as path from "path";
+import * as fs from "fs";
+import http from "http";
+import express from "express";
+const app = express();
 
-import * as express from 'express';
-const app = express()
-
-import factory from './lib/Factory';
-import { api } from './routes/api';
+import factory from "./lib/Factory";
+import { api } from "./routes/api";
 
 const setupServer = () => {
   const SERVER_PORT = process.env.MONGOKU_SERVER_PORT || 3100;
 
-  app.get('/', (req, res, next) => {
+  app.get("/", (req, res, next) => {
     res.sendFile("app/index.html", { root: __dirname }, (err) => {
       if (err) {
         return next(err);
@@ -18,9 +18,9 @@ const setupServer = () => {
     });
   });
 
-  app.use('/api', api);
+  app.use("/api", api);
 
-  app.get('/*', (req, res, next) => {
+  app.get("/*", (req, res, next) => {
     const ext = path.extname(req.url);
 
     fs.stat(path.join(__dirname, "app", req.url), (err, stats) => {
@@ -41,13 +41,17 @@ const setupServer = () => {
     res.status(500);
 
     return res.json({
-      ok:      false,
-      message: err.message
+      ok: false,
+      message: err.message,
     });
-  })
+  });
 
-  app.listen(SERVER_PORT, () => console.log(`[Mongoku] listening on port `+SERVER_PORT));
-}
+  server.listen(SERVER_PORT, () =>
+    console.log(`[Mongoku] listening on port ` + SERVER_PORT)
+  );
+};
+
+const server = http.createServer(app);
 
 export const start = async () => {
   console.log(`[Mongoku] Starting...`);
